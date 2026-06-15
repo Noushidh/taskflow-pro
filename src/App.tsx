1360a47ca71d816847f21c9ecfc3e71b1904e8cd
noushidh <noushidhk@gmail.com>
2026-06-15 17:56:18 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import Greet from './components/Home'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500">
+  
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+ function Greet(){
+  
+    return(
+        <>
+        <button>click</button>
+        </>
+    )
+}
+export default Greet
