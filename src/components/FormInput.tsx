2eb7a66d40aacd927452cbe0eafa5f5ae0056b5d
noushidh <noushidhk@gmail.com>
2026-06-16 22:21:45 +0530

    style(form): update custom input color scheme

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -8,38 +8,45 @@ function FormInput({
   type = "text",
 }: FormInputProps) {
   return (
-    <div className="relative mt-6">
-      <input
-        type={type}
-        placeholder=" "
-        className="
-          peer
-          w-full
-          p-4
-          rounded-2xl
-          bg-transparent
-          border
-          border-slate-500
-          text-white
-          focus:outline-none
-          focus:border-purple-500
-        "
-      />
+<div className="relative mt-6">
+  <input
+    type={type}
+    placeholder=" "
+    className="
+      peer
+      w-full
+      p-4
+      rounded-2xl
+      bg-transparent
+      border
+      border-black
+      text-black
+      focus:outline-none
+      focus:border-black
+    "
+  />
 
-      <label
-  className="
-    absolute
-    left-4
-    -top-3
-    px-2
-    text-sm
-    text-purple-500
-    bg-[oklch(0.61_0.11_210.56)]
-  "
-      >
-        {label}
-      </label>
-    </div>
+  <label
+    className="
+      absolute
+      left-4
+      px-2
+      bg-[oklch(0.61_0.11_210.56)]
+      transition-all
+      duration-200
+
+      peer-placeholder-shown:top-4
+      peer-placeholder-shown:text-base
+
+      peer-focus:-top-3
+      peer-focus:text-sm
+
+      text-black
+    "
+  >
+    {label}
+  </label>
+</div>
   );
 }
 
