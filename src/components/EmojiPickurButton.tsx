ae41e58b2115749d94558c7424ac1107faa1ee8d
noushidh <noushidhk@gmail.com>
2026-06-16 19:59:03 +0530

    implement hide and show logic in the button of add and edit

--- src/components/EmojiPickurButton.tsx
+++ src/components/EmojiPickurButton.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+import { div, em } from "framer-motion/client";
 
 function EmojiPickerButton() {
+  const [emoji, setEmoji] = useState("😀");
+  const [showPicker, setShowPicker] = useState(false);
+
   return (
-<div className="relative w-40 h-40 mx-auto">
-  {/* Main Emoji Button */}
-  <button
-    className="
+    <div className="relative w-40 h-40 mx-auto">
+      {/* Main Emoji Button */}
+      <button
+        className="
       w-32 h-32
       rounded-full
       bg-gradient-to-br from-fuchsia-500 to-purple-600
@@ -15,29 +21,55 @@ function EmojiPickerButton() {
       left-1/2 top-1/2
       -translate-x-1/2 -translate-y-1/2
     "
-  >
-    😀
-  </button>
+      >
+        {emoji}
+      </button>
 
-  {/* Small Plus Button */}
-  <button
-    className="
+      {/* Small Plus Button */}
+      <button
+        className="
       absolute
       top-6
       right-4
       w-10 h-10
       rounded-full
-      bg-white
-      text-purple-600
+      bg-slate-500/80
+      text-white
       font-bold
       text-2xl
       flex items-center justify-center
       shadow-lg
+      cursor-pointer
     "
-  >
-    +
-  </button>
-</div>
+        onClick={() => setShowPicker(true)}
+      >
+  {emoji === "😀" ? "+" : "✏️"}
+      </button>
+      {showPicker && (
+        <div>
+          <EmojiPicker
+            onEmojiClick={(emojiData) => {
+              setEmoji(emojiData.emoji);
+              setShowPicker(false);
+            }}
+          />
+
+    <button
+      onClick={() => setShowPicker(false)}
+      className="
+        mt-2
+        px-4 py-2
+        rounded-lg
+        bg-red-500
+        text-white
+        cursor-pointer
+      "
+    >
+      Cancel
+    </button>
+        </div>
+      )}
+    </div>
   );
 }
 
