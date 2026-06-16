75c6b5bea4e31a299233f9a6f764c2e600548cc1
noushidh <noushidhk@gmail.com>
2026-06-16 12:58:10 +0530

    feat(home): add floating action button for creating tasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Greet from './components/Home'
 function App() {
   return (
     <div className="min-h-screen bg-red-500">
-  
+        <Greet/>
     </div>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 
- function Greet(){
-  
-    return(
-        <>
-        <button>click</button>
-        </>
-    )
+function Greet() {
+  return (
+    <>
+      <button  className="
+        fixed bottom-6 right-6
+        w-16 h-16
+        rounded-full
+        bg-green-400
+        hover:bg-green-600
+        text-white
+        text-4xl
+        flex items-center justify-center
+        shadow-lg
+        transition
+        right-50
+        bottom-10
+      ">
+        +
+      </button>
+    </>
+  );
 }
-export default Greet
+export default Greet;
